44b7bb5008940962189531fccf400a208b99c998
wwagner4 <wwagner4@gmail.com>
2020-09-20 06:08:15 +0200

    improved bar chart example

--- src/main/scala/entelijan/viz/examples/Barchart.scala
+++ src/main/scala/entelijan/viz/examples/Barchart.scala
@@ -7,12 +7,29 @@ object Barchart extends App {
 
   val c: VizCreator[XY] = VizCreators.gnuplot(clazz = classOf[XY])
 
-  val dats = (-10 to 10).map(i => XY(i, math.exp(i * 0.1)))
+  val dats1 = Seq(
+    XY(0, 10),
+    XY(1, 11),
+    XY(2, 12),
+    XY(3, 9),
+  )
+  val dats2 = Seq(
+    XY(0, 8),
+    XY(1, 6),
+    XY(2, 5),
+    XY(3, 1),
+  )
+
+  val rows = Seq(
+    DataRow(data = dats1, style = Style_BOXES),
+    DataRow(data = dats2, style = Style_BOXES),
+  )
 
   val dia = Diagram[XY](
     id = "ex5",
     title = "Example 5",
-    dataRows = List(DataRow(data = dats, style = Style_BOXES)),
+    yRange = Some(Range(Some(0), None)),
+    dataRows = rows,
   )
 
   c.createDiagram(dia)
